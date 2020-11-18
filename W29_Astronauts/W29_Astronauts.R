7a59c35570b0dc12cfb9d395b1d3a807578e4184
Zetluis <luisfrein@gmail.com>
2020-11-18 12:04:40 -0400

    Fixed a typo.

--- W29_Astronauts/W29_Astronauts.R
+++ W29_Astronauts/W29_Astronauts.R
@@ -15,7 +15,7 @@ plot <- astronauts %>%
   geom_point(aes(color = sex, fill = sex), pch = 21, stroke = 1.5, size = 5) +
   scale_color_manual(labels = c("Male", "Female"), values = c("#ECC761", "#8A8EF9"), limits = c("male", "female")) +
   scale_fill_manual(labels = c("Male", "Female"), values = c("#FD6F03", "#EBECFE"), limits = c("male", "female")) +
-  labs(title = "**Out in the Vacumn of Space**", subtitle = "This 15 astronauts,
+  labs(title = "**Out in the Vacumn of Space**", subtitle = "These 15 astronauts,
        <span style='color:#FD6F03;'>male</span> and
        <span style='color:#8a8ef9;'>female</span>,
        spent the most time in</span><br>ExtravehicularActivity (activity done outside a spacecrat).", caption = "Made by @luisfreii | Data:  Mariya Stavnichuk and Tatsuya Corlett", x = "Total Extravehicular Hours", y = NULL)
