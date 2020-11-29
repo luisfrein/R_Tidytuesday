407117ebed57936e73ed98fd6db55e3df39f81db
Luis Freites <luisfrein@gmail.com>
2020-11-29 14:24:36 -0500

    Update W15_Tour_de_France.R

--- W15_Tour_de_France/W15_Tour_de_France.R
+++ W15_Tour_de_France/W15_Tour_de_France.R
@@ -8,15 +8,23 @@ tuesdata <- tidytuesdayR::tt_load(2020, week = 15)
 tdf_winners <- tuesdata$tdf_winners
 
 #Expore!
+#Create table
 tdf_table <- tdf_winners %>% 
   filter(!is.na(time_overall)) %>% 
-  mutate(start_date = lubridate::year(start_date)) %>% 
-  slice_tail(n = 15) %>% 
-  select(3, 4, 19, 2, 1, 8, 5:6) %>% 
+  #Filter out NAs.
+  mutate(start_date = lubridate::year(start_date), nationality = str_trim(nationality),
+         nationality = recode_factor(nationality,'United States' = 'United%20States'),
+         nationality = recode_factor(nationality,'Great Britain' = 'Great%20Britain')
+         ) %>% 
+  #Change factors names, to get the correct url in the next mutate.
+  slice_tail(n = 20) %>% 
+  mutate(img = paste0("https://github.com/Zetluis/R_Tidytuesday/blob/master/W15_Tour_de_France/Flag%20icons/",nationality, ".png?raw=true")) %>%
+  #Pastes the url for each nationality.
+  select(3, 4, 20, 2, 1, 5:6) %>% 
   gt() 
   
-
-tdf_table %>%
+#Download images for the table, use HTML for the winner column and change columns names.
+tdf_table <- tdf_table %>%
   cols_merge(
     columns = vars(winner_name, winner_team)
 ) %>% 
@@ -28,23 +36,47 @@ tdf_table %>%
       name <- word(x, 1, 2)
       team <- word(x, 3, -1)
       glue::glue(
-        "<div><span style='font-weight:bold;font-variant:small-caps;font-size:14px'>{name}</div>
-        <div><span style ='font-weight:bold;color:grey;font-size:10px'>{team}</span></div>"
+        "<div style='line-height:10px'><span style='font-weight:bold;font-variant:small-caps;font-size:14px'>{name}</div>
+        <div style='line-height:12px'><span style ='font-weight:bold;color:grey;font-size:10px'>{team}</span></div>"
       )
       }
+  ) %>% 
+  text_transform(
+    locations = cells_body(vars(img)),
+    fn = function(x){
+      web_image(url = x, height = 20)
+    }) %>% 
+  cols_label(
+    winner_name = md("**Winner**"),
+    img = "",
+    start_date = md("**Year**"),
+    edition = md("**Edition**"),
+    distance = md("**Distance (KM)**"),
+    time_overall = md("**Overall Hours**")
   )
 
+#Add style to the table
 tdf_table %>% 
   tab_header(
-    title = md("**Tour de France Winners of the Past 15 Years**"),
-    subtitle = html("<em>The British and Spaniards have dominated these last years.</em>")
+    title = md("**Tour de France Winners 2000-2019**"),
     ) %>% 
   fmt_number(
     columns = 6:7,
-    decimals = 2
+    decimals = 0
   ) %>% 
   tab_source_note("Made by @luisfreii | Data: Alastair Rushworth's") %>% 
+  tab_style(
+    style = list(
+      cell_text(font = "Arial Rounded MT Bold", weight = "normal")),
+    locations = 
+      cells_column_labels(columns = gt::everything()
+      )
+    ) %>% 
   tab_options(
-    column_labels.border.bottom.color = "black",
-    column_labels.border.bottom.width = px(3)
-  )
+    table.border.top.color = "#080D0A",
+    column_labels.border.top.color = "#080D0A",
+    column_labels.border.bottom.color = "#080D0A",
+    column_labels.border.bottom.width = px(3),
+    column_labels.background.color = "#080D0A",
+    table_body.hlines.color = "#ADADAD"
+  ) 
