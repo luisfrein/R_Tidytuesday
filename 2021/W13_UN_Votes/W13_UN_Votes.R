7327938c916a9de8552c4526f92686482d96dfa3
Zetluis <luisfrein@gmail.com>
2021-03-23 11:51:43 -0400

    Finished week 13 and added plot

--- 2021/W13_UN_Votes/W13_UN_Votes.R
+++ 2021/W13_UN_Votes/W13_UN_Votes.R
@@ -2,53 +2,57 @@
 library(tidyverse)
 library(tidytext)
 library(widyr)
-library(extrafont)
 library(igraph)
 library(ggraph)
 
 #Get the data
 tuesdata <- tidytuesdayR::tt_load(2021, week = 13)
 
-unvotes <- tuesdata$unvotes
 roll_calls <-  tuesdata$roll_calls
 
-#Explore!
-unvotes %>% 
-  filter(str_detect(country, "Venezuela")) -> ven_votes
-
-ven_votes %>% 
-  left_join(roll_calls) -> ven_votes
-
-ven_votes %>% 
-  filter(!vote == "abstain") %>% 
-  unnest_tokens(word, short) %>% 
-  anti_join(stop_words) -> ven_votes
-
-ven_votes %>% 
-  filter(!is.na(word)) %>% 
-  group_by(vote) %>% 
-  count(word, sort = TRUE) 
-
-#Word correlation
+#Unnest desc column and remove common words 
 roll_calls %>% 
-  unnest_tokens(word, short) %>% 
+  unnest_tokens(word, descr) %>% 
   anti_join(stop_words) -> roll_calls_words
 
-roll_calls_words %>% 
-  pairwise_count(word, session, sort = TRUE) -> word_pairs
-
+#Correlation between words
 roll_calls_words %>% 
   group_by(word) %>% 
   filter(!is.na(word), n() > 20) %>% 
   pairwise_cor(word, session, sort = TRUE) -> word_cors
 
+#Vector of colors
+colores <- 
+  c(
+    line = "#9D0208",
+    point = "#FAA307",
+    bkg = "#03071E"
+  )
+
+#aRt (?)
 set.seed(1512)
 
 word_cors %>%
-  filter(correlation > .73) %>%
+  filter(correlation > .5, item1 %in% c("nuclear", "weapons", "treaty")) %>%
   graph_from_data_frame() %>%
-  ggraph(layout = "fr") +
-  geom_edge_link(aes(edge_alpha = correlation), show.legend = FALSE) +
-  geom_node_point(color = "lightblue", size = 5) +
-  geom_node_text(aes(label = name), repel = TRUE) +
-  theme_void()
+  ggraph(layout = "dh") +
+  geom_edge_link(aes(edge_alpha = correlation), show.legend = FALSE, color = colores[["line"]]) +
+  geom_node_point(color = colores[["point"]], size = 3) +
+  coord_polar() +
+  theme_void() +
+  theme(panel.background = element_rect(fill = colores[["bkg"]]),
+        plot.background = element_rect(fill = colores[["bkg"]]))
+
+#Code to save the plot. I went for SVG, and then used InkScape to convert to PNG for better Antialiasing
+# ggsave("UN_art.png",
+#        width = 22,
+#        height = 15,
+#        dpi = 500,
+#        units = "cm",
+#        type = "cairo-png")
+
+# ggsave("UN_art.svg",
+#        width = 22,
+#        height = 15,
+#        dpi = 500,
+#        units = "cm")
