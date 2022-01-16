3ad91cc1470d8d5abb18df51e40041ce834691f9
luisfrein <luisfrein@gmail.com>
2022-01-16 15:32:40 -0400

    Finished W02 2022

--- 2022/W02_Bee_Colonies/W02_Bee_Colonies.R
+++ 2022/W02_Bee_Colonies/W02_Bee_Colonies.R
@@ -1,24 +1,62 @@
 #Load packages
 library(tidyverse)
-library(extrafont)
+library(showtext)
+library(scales)
 
 #Get the data
 tuesdata <- tidytuesdayR::tt_load('2022-01-11')
 colony <- tuesdata$colony
 
+#Get font
+font_add_google('IBM Plex Sans', family = 'ibm')
+
+## Automatically use showtext to render text for future devices
+showtext_auto()
+showtext_opts(dpi = 300)
 
 #Get to work
 colony %>% 
-  filter(!is.na(colony_lost_pct),
-         !is.na(colony_reno_pct),
-         !state == 'United States', 
-         year == 2021) %>% 
-  mutate(year_result = case_when(colony_reno_pct - colony_lost_pct > 0 ~ 'net_reno',
-                                 TRUE ~ 'net_lost')) %>% 
-  pivot_longer(2:3, names_to = 'mean_lost_added', values_to = 'values') -> colony_means
+  filter(!is.na(colony_added),
+         !is.na(colony_lost)) %>% 
+  group_by(year) %>% 
+  summarise(colonies_added = sum(colony_added),
+            colonies_lost = sum(colony_lost)) %>% 
+  mutate(fill_cond = case_when(year == 2019 | year == 2021 ~ 'highlight',
+                                TRUE ~ 'normal')) %>% 
+  ggplot() +
+  geom_col(aes(year, colonies_added, fill = fill_cond), show.legend = FALSE) +
+  scale_y_continuous(labels = label_comma()) +
+  scale_x_continuous(breaks = breaks_pretty(n = 7)) +
+  scale_fill_manual(values = c('#deb841', '#6d6a75')) +
+  labs(title = 'Number of Honey Bees colonies added in the US from 2015 to 2021',
+       subtitle = 'The number of colonies added remained stable until 2019. That year saw\na significant collapse of colonies, a similar pattern repeated in 2021.',
+       y = 'Colonies Added',
+       x = NULL,
+       caption = 'Visualization: **@luisfreii** | Source: **United States Department of Agriculture**') +
+  theme(axis.text = element_text(family = 'ibm',
+                                 size = 9),
+        axis.title = element_text(family = 'ibm',
+                                  size = 9),
+        plot.title = element_text(family = 'ibm',
+                             size = 12),
+        plot.subtitle = element_text(family = 'ibm',
+                                     size = 9),
+        plot.caption = ggtext::element_markdown(family = 'ibm',
+                                    size = 6),
+        panel.background = element_rect('#F5F5F5'),
+        plot.background = element_rect('#F5F5F5'),
+        panel.grid = element_blank(),
+        axis.ticks.y = element_blank(),
+        plot.title.position = 'plot',
+        plot.caption.position = 'plot',
+        plot.margin = margin(25, 15, 10, 15))
 
-colony_means %>% 
-  ggplot(aes(values, fct_reorder(state, values), color = mean_lost_added)) +
-  geom_line(aes(group = state), size = 1, color = '#525252') +
-  geom_point(size = 4, show.legend = FALSE) +
-  scale_color_manual(values = c('black', 'black'))
+ggsave('W02_Bee_colonies.png',
+       width = 18,
+       heigh = 10,
+       units = 'cm',
+       dpi = 300,
+       type = 'cairo')
+
+
+
